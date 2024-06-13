30257aea2082ccc79db4e4add70519c0d2f3fbd4
Shun Okawa <shunokawa@gmail.com>
2024-06-14 01:43:16 +0900

    feat: task_category_mastersのseed追加

---
+++ seeds/task_category_masters_seed.sql
@@ -0,0 +1,4 @@
+-- InsertTable
+INSERT INTO task_category_masters (name) VALUES ('Work');
+INSERT INTO task_category_masters (name) VALUES ('Study');
+INSERT INTO task_category_masters (name) VALUES ('Other');
