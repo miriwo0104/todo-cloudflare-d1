db0ebd712f68592d978b9443e4b69eab4b6dbd94
Shun Okawa <shunokawa@gmail.com>
2024-06-14 02:11:24 +0900

    feat: seedまとめスクリプトと呼び出しnpmコマンド追加

---
+++ execute_seeds.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# SQLファイルのリスト
+sql_files=(
+    # NOTE: seedファイルを追加したらこちらにも追加
+    "./seeds/task_category_masters_seed.sql"
+)
+
+# 各SQLファイルを実行
+for file in "${sql_files[@]}"; do
+    npx wrangler d1 execute todo-cloudflare-d1 --file "$file"
+done
