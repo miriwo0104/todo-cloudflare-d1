673a47ab4d7acb3e941360c62764bc1e58b53904
Shun Okawa <shunokawa@gmail.com>
2024-06-15 14:50:32 +0900

    feat: 一覧に論理削除されたTODOが表示されないように変更

--- app/routes/tasks-loader.ts
+++ app/routes/tasks-loader.ts
@@ -2,7 +2,9 @@ import type { LoaderFunctionArgs} from "@remix-run/cloudflare";
 
 export const loader = async ({ context }: LoaderFunctionArgs) => {
   try {
-    return await context.db.tasks.findMany();
+    return await context.db.tasks.findMany({
+      where: { deleted_at: null },
+    });
   } catch (error) {
     console.error("Failed to load tasks:", error);
     throw new Response("Internal Server Error", { status: 500 });
