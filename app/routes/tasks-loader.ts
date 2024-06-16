6a1007fc4202a6c81fba77f582ab060a62c84259
Shun Okawa <shunokawa@gmail.com>
2024-06-16 10:22:10 +0900

    feat: クエリパラメーターで絞り込み

--- app/routes/tasks-loader.ts
+++ app/routes/tasks-loader.ts
@@ -1,10 +1,19 @@
 import type { LoaderFunctionArgs } from "@remix-run/cloudflare";
 
-export const loader = async ({ context }: LoaderFunctionArgs) => {
+export const loader = async ({ context, request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const searchParams = url.searchParams;
+  const isComplete = searchParams.get("isComplete");
+  const isTrue = 1; // フラグONの意味 マジックナンバー申し訳ない。。
+
   try {
-    return await context.db.tasks.findMany({
-      where: { deleted_at: null },
+    const tasks = await context.db.tasks.findMany({
+      where: {
+        deleted_at: null,
+        ...(isComplete !== null && { is_complete: isTrue }),
+      },
     });
+    return tasks;
   } catch (error) {
     console.error("Failed to load tasks:", error);
     throw new Response("Internal Server Error", { status: 500 });
