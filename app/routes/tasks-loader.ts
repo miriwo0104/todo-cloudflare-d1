e4f56ba810e2b75f078496a0efb63906e242aaee
Shun Okawa <shunokawa@gmail.com>
2024-07-04 22:59:48 +0900

    feat: クエリパラメーターでページ情報を受け取り

--- app/routes/tasks-loader.ts
+++ app/routes/tasks-loader.ts
@@ -13,6 +13,9 @@ export const loader = async ({ context, request }: LoaderFunctionArgs) => {
   const taskCategoryMasterIdParams: string[] = searchParams.getAll("taskCategoryMasterId");
   const taskCategoryMasterIds: number[] = taskCategoryMasterIdParams.map(id => Number(id));
 
+  const page: number = Number(searchParams.get("page")) || 1; // 設定されていなかったらデフォルト値の1を設定
+  const pageSize: number = 5; // 1ページあたりの表示数
+
   const isTrue = 1; // フラグONの意味 マジックナンバー申し訳ない。。
 
   try {
@@ -25,6 +28,8 @@ export const loader = async ({ context, request }: LoaderFunctionArgs) => {
       include: {
         task_category_master: true, // schema.prismaで定義したリレーションを指定
       },
+      skip: (page - 1) * pageSize, // スキップするレコード数
+      take: pageSize, // 取得するレコード数
     });
     return tasks;
   } catch (error) {
