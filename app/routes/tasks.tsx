8a12c72d728aff8ef295c9ff0fcd5145867c13cf
Shun Okawa <shunokawa@gmail.com>
2024-07-06 13:57:45 +0900

    feat: ページネーションのリンクを追加

--- app/routes/tasks.tsx
+++ app/routes/tasks.tsx
@@ -21,6 +21,11 @@ export default function Index() {
   const pageSize = taskListInfos.pageSize;
   const nowPage = taskListInfos.nowPage;
   const totalPage = Math.ceil(tasksCount / pageSize); // 切り上げで整数を返す
+  const existsBeforePage = nowPage !== 1; // 現在のページが1ページ目ではない場合前ページが有る
+  const beforePage = nowPage - 1; // 前ページのページ番号
+  const existsAfterPage = nowPage !== totalPage; // 現在のページが最終ページではない場合次ページが有る
+  const afterPage = nowPage + 1; // 次ページのページ番号
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>tasks</h1>
@@ -38,7 +43,23 @@ export default function Index() {
         ))}
       </ul>
       <div>
-        <p>{nowPage} / {totalPage}</p>
+        <div>
+          { existsBeforePage
+            ? <a href={`?page=${beforePage}`}>
+                前ページへ
+              </a>
+            : null
+          }
+        </div>
+        <div>{nowPage} / {totalPage}</div>
+        <div>
+          { existsAfterPage
+            ? <a href={`?page=${afterPage}`}>
+                次ページへ
+              </a>
+            : null
+          }
+        </div>
       </div>
     </div>
   );
